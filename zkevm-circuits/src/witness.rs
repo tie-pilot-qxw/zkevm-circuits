1b765c5dbd11c1bc29cc2b468b82e14f05c3e842
Yanpei Guo <2847399255@qq.com>
2023-07-18 16:05:57 +0800

    add witness struct (merge request !23)
    Squash merge branch 'gyp-witness' into 'develop'
    
    * cargo fmt
    
    * fix type mistake
    
    * add witness struct

--- zkevm-circuits/src/witness.rs
+++ zkevm-circuits/src/witness.rs
@@ -9,3 +9,21 @@ pub mod state;
 
 pub use block::Block;
 pub use block::{EXECUTION_STATE_NUM, OPERAND_NUM};
+use trace_parser::Trace;
+
+pub struct Witness {
+    bytecode: Vec<bytecode::Row>,
+    copy: Vec<copy::Row>,
+    core: Vec<core::Row>,
+    exp: Vec<exp::Row>,
+    public: Vec<public::Row>,
+    state: Vec<state::Row>,
+}
+
+struct CurrentState {}
+
+impl Witness {
+    fn new(trace: Vec<Trace>) -> Self {
+        unimplemented!()
+    }
+}
