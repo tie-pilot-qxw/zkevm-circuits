289d1df52d58f68f5a825c7dfd323ad427b1d077
geruiwang <wang.gerui@foxmail.com>
2023-07-26 16:35:11 +0800

    better dynamic selector (merge request !41)
    Squash merge branch 'gerui-better-dynamicselector' into 'develop'
    
    * in dynamic_selector.rs use array instead of vec
    
    * add length parameter to DynamicSelectorConfig type definition.

--- gadgets/src/dynamic_selector.rs
+++ gadgets/src/dynamic_selector.rs
@@ -16,36 +16,43 @@ use std::marker::PhantomData;
 
 /// Dynamic selector that generates expressions of degree 2 to select from N
 /// possible targets using about 2 sqrt(N) cells..
+/// Type parameter CNT: how many possible targets this selector supports.
 #[derive(Clone, Debug)]
-pub struct DynamicSelectorConfig<F> {
-    #[allow(dead_code)]
-    /// N value: how many possible targets this selector supports.
-    count: usize,
-    /// target = i * low_len + j
+pub struct DynamicSelectorConfig<F, const CNT: usize, const NUM_HIGH: usize, const NUM_LOW: usize> {
+    /// target = i * NUM_LOW + j
     /// selector = high[i] * low[j]
-    target_high: Vec<Column<Advice>>,
-    target_low: Vec<Column<Advice>>,
+    target_high: [Column<Advice>; NUM_HIGH],
+    target_low: [Column<Advice>; NUM_LOW],
     _marker: PhantomData<F>,
 }
 
-impl<F: Field> DynamicSelectorConfig<F> {
+impl<F: Field, const CNT: usize, const NUM_HIGH: usize, const NUM_LOW: usize>
+    DynamicSelectorConfig<F, CNT, NUM_HIGH, NUM_LOW>
+{
     /// Annotates columns of this gadget embedded within a circuit region.
     pub fn annotate_columns_in_region(&self, region: &mut Region<F>, prefix: &str) {
         self.target_high
             .iter()
             .enumerate()
             .for_each(|(index, col)| {
-                region.name_column(|| format!("{}_high_{}", prefix, index), *col)
+                region.name_column(
+                    || format!("{}_DYNAMIC_SELECTOR_high_{}", prefix, index),
+                    *col,
+                )
             });
         self.target_low.iter().enumerate().for_each(|(index, col)| {
-            region.name_column(|| format!("{}_low_{}", prefix, index), *col)
+            region.name_column(
+                || format!("{}_DYNAMIC_SELECTOR_low_{}", prefix, index),
+                *col,
+            )
         });
     }
 
     /// Get the selector value of target
     pub fn selector(&self, meta: &mut VirtualCells<F>, target: usize) -> Expression<F> {
-        let high = target / self.target_low.len();
-        let low = target % self.target_low.len();
+        assert!(target < NUM_LOW * NUM_HIGH);
+        let high = target / NUM_LOW;
+        let low = target % NUM_LOW;
         let target_high = meta.query_advice(self.target_high[high], Rotation::cur());
         let target_low = meta.query_advice(self.target_low[low], Rotation::cur());
         target_high * target_low
@@ -53,29 +60,35 @@ impl<F: Field> DynamicSelectorConfig<F> {
 }
 
 /// Wrapper arround [`DynamicSelectorChip`] for which [`Chip`] is implemented.
-pub struct DynamicSelectorChip<F> {
-    config: DynamicSelectorConfig<F>,
+pub struct DynamicSelectorChip<F, const CNT: usize, const NUM_HIGH: usize, const NUM_LOW: usize> {
+    config: DynamicSelectorConfig<F, CNT, NUM_HIGH, NUM_LOW>,
 }
 
-impl<F: Field> DynamicSelectorChip<F> {
+impl<F: Field, const CNT: usize, const NUM_HIGH: usize, const NUM_LOW: usize>
+    DynamicSelectorChip<F, CNT, NUM_HIGH, NUM_LOW>
+{
     /// Sets up the configuration of the chip, and own the necessary columns
     pub fn configure(
         meta: &mut ConstraintSystem<F>,
         q_enable: impl FnOnce(&mut VirtualCells<'_, F>) -> Expression<F>,
-        count: usize,
-        target_high: Vec<Column<Advice>>,
-        target_low: Vec<Column<Advice>>,
-    ) -> DynamicSelectorConfig<F> {
-        assert!(target_high.len() * target_low.len() >= count);
+        target_high: [Column<Advice>; NUM_HIGH],
+        target_low: [Column<Advice>; NUM_LOW],
+    ) -> DynamicSelectorConfig<F, CNT, NUM_HIGH, NUM_LOW> {
+        assert!(NUM_HIGH * NUM_LOW >= CNT);
+        let config = DynamicSelectorConfig {
+            target_high,
+            target_low,
+            _marker: Default::default(),
+        };
         meta.create_gate("dynamic_selector gate", |meta| {
             let q_enable = q_enable(meta);
-            let mut constraints: Vec<(&str, Expression<F>)> = target_high
+            let mut constraints: Vec<(String, Expression<F>)> = target_high
                 .iter()
                 .chain(target_low.iter())
                 .map(|col| {
                     let x = meta.query_advice(col.clone(), Rotation::cur());
                     (
-                        "selector bool",
+                        "selector bool".into(),
                         q_enable.clone() * x.clone() * (1u8.expr() - x),
                     )
                 })
@@ -85,23 +98,31 @@ impl<F: Field> DynamicSelectorChip<F> {
                 .map(|col| meta.query_advice(col.clone(), Rotation::cur()))
                 .reduce(|acc, expr| acc + expr)
                 .unwrap();
-            constraints.push(("sum_high = 1", q_enable.clone() * (sum_high - 1u8.expr())));
+            constraints.push((
+                "sum_high = 1".into(),
+                q_enable.clone() * (sum_high - 1u8.expr()),
+            ));
             let sum_low = target_low
                 .iter()
                 .map(|col| meta.query_advice(col.clone(), Rotation::cur()))
                 .reduce(|acc, expr| acc + expr)
                 .unwrap();
-            constraints.push(("sum_low = 1", q_enable.clone() * (sum_low - 1u8.expr())));
+            constraints.push((
+                "sum_low = 1".into(),
+                q_enable.clone() * (sum_low - 1u8.expr()),
+            ));
+            // usable targets are 0..=CNT-1. unusable targets are CNT..=NUM_LOW*NUM_HIGH-1
+            for unusable in CNT..NUM_LOW * NUM_HIGH {
+                constraints.push((
+                    format!("unusable target {} selector = 0", unusable),
+                    q_enable.clone() * config.selector(meta, unusable),
+                ));
+            }
 
             constraints
         });
 
-        DynamicSelectorConfig {
-            count,
-            target_high,
-            target_low,
-            _marker: Default::default(),
-        }
+        config
     }
 
     /// Assign the DynamicSelectorChip
@@ -111,13 +132,13 @@ impl<F: Field> DynamicSelectorChip<F> {
         offset: usize,
         target: usize,
     ) -> Result<(), Error> {
-        let high = target / self.config.target_low.len();
-        let low = target % self.config.target_low.len();
+        let high = target / NUM_LOW;
+        let low = target % NUM_LOW;
 
         for (index, col) in self.config.target_high.iter().enumerate() {
             region.assign_advice(
                 || format!("target_high {}", index),
-                col.clone(),
+                *col,
                 offset,
                 || Value::known(if index == high { F::ONE } else { F::ZERO }),
             )?;
@@ -125,7 +146,7 @@ impl<F: Field> DynamicSelectorChip<F> {
         for (index, col) in self.config.target_low.iter().enumerate() {
             region.assign_advice(
                 || format!("target_low {}", index),
-                col.clone(),
+                *col,
                 offset,
                 || Value::known(if index == low { F::ONE } else { F::ZERO }),
             )?;
@@ -134,13 +155,15 @@ impl<F: Field> DynamicSelectorChip<F> {
     }
 
     /// Given an `DynamicSelectorChip`, construct the chip.
-    pub fn construct(config: DynamicSelectorConfig<F>) -> Self {
+    pub fn construct(config: DynamicSelectorConfig<F, CNT, NUM_HIGH, NUM_LOW>) -> Self {
         Self { config }
     }
 }
 
-impl<F: Field> Chip<F> for DynamicSelectorChip<F> {
-    type Config = DynamicSelectorConfig<F>;
+impl<F: Field, const CNT: usize, const NUM_HIGH: usize, const NUM_LOW: usize> Chip<F>
+    for DynamicSelectorChip<F, CNT, NUM_HIGH, NUM_LOW>
+{
+    type Config = DynamicSelectorConfig<F, CNT, NUM_HIGH, NUM_LOW>;
     type Loaded = ();
 
     fn config(&self) -> &Self::Config {
@@ -166,21 +189,23 @@ mod test {
     use std::marker::PhantomData;
 
     #[derive(Clone, Debug)]
-    struct TestCircuitConfig<F> {
+    struct TestCircuitConfig<F, const CNT: usize, const NUM_HIGH: usize, const NUM_LOW: usize> {
         q_enable: Selector,
         /// covert target values to 0/1 table by brutal force, for test check
         zero_one_values: Vec<Column<Fixed>>,
-        dynamic_selector: DynamicSelectorConfig<F>,
+        dynamic_selector: DynamicSelectorConfig<F, CNT, NUM_HIGH, NUM_LOW>,
     }
 
     #[derive(Default)]
-    struct TestCircuit<F: Field, const COUNT: usize> {
+    struct TestCircuit<F: Field, const CNT: usize, const NUM_HIGH: usize, const NUM_LOW: usize> {
         values: Vec<u64>,
         _marker: PhantomData<F>,
     }
 
-    impl<F: Field, const COUNT: usize> Circuit<F> for TestCircuit<F, COUNT> {
-        type Config = TestCircuitConfig<F>;
+    impl<F: Field, const CNT: usize, const NUM_HIGH: usize, const NUM_LOW: usize> Circuit<F>
+        for TestCircuit<F, CNT, NUM_HIGH, NUM_LOW>
+    {
+        type Config = TestCircuitConfig<F, CNT, NUM_HIGH, NUM_LOW>;
         type FloorPlanner = SimpleFloorPlanner;
 
         fn without_witnesses(&self) -> Self {
@@ -189,26 +214,22 @@ mod test {
 
         fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
             let q_enable = meta.complex_selector();
-            let lo_cnt = (COUNT as f64).sqrt().floor();
-            let hi_cnt = ((COUNT as f64) / lo_cnt).floor();
-            let target_low = (0..lo_cnt as usize).map(|_| meta.advice_column()).collect();
-            let target_high: Vec<Column<Advice>> =
-                (0..hi_cnt as usize).map(|_| meta.advice_column()).collect();
+            let target_low: [_; NUM_LOW] = std::array::from_fn(|_| meta.advice_column());
+            let target_high: [_; NUM_HIGH] = std::array::from_fn(|_| meta.advice_column());
             let dynamic_selector = DynamicSelectorChip::configure(
                 meta,
                 |meta| meta.query_selector(q_enable),
-                COUNT,
                 target_high,
                 target_low,
             );
             let zero_one_values: Vec<Column<Fixed>> =
-                (0..COUNT).map(|_| meta.fixed_column()).collect();
+                (0..CNT).map(|_| meta.fixed_column()).collect();
 
             // gate for test
             meta.create_gate("test gate", |meta| {
                 let q_enable = meta.query_selector(q_enable);
                 let mut constraints: Vec<(&str, Expression<F>)> = vec![];
-                for target in 0..COUNT {
+                for target in 0..CNT {
                     let expr = dynamic_selector.selector(meta, target);
                     let expr_should_be = meta.query_fixed(zero_one_values[target], Rotation::cur());
                     constraints.push((
@@ -238,7 +259,7 @@ mod test {
                     for (offset, value) in self.values.iter().enumerate() {
                         config.q_enable.enable(&mut region, offset)?;
                         dynamic_selector.assign(&mut region, offset, *value as usize)?;
-                        for target in 0..COUNT {
+                        for target in 0..CNT {
                             region.assign_fixed(
                                 || "zero_one_values",
                                 config.zero_one_values[target],
@@ -260,23 +281,39 @@ mod test {
     }
 
     macro_rules! gen_try_test_circuit {
-        ($count:expr) => {
-            fn try_test_circuit(values: Vec<u64>) {
-                let circuit = TestCircuit::<Fp, $count> {
+        ($($y:expr),+) => {
+            fn try_test_circuit(values: Vec<u64>, is_err: bool) {
+                let circuit = TestCircuit::<Fp, $($y),+> {
                     values: values,
                     _marker: PhantomData,
                 };
                 let k = 9;
                 let prover = MockProver::<Fp>::run(k, &circuit, vec![]).unwrap();
-                prover.assert_satisfied_par()
+                if !is_err {
+                    prover.assert_satisfied_par();
+                } else {
+                    let res = prover.verify_par();
+                    assert!(res.is_err());
+                    println!("{:?}", res);
+                }
             }
         };
     }
 
     #[test]
     fn correct_expr_of_selector() {
-        const COUNT: u64 = 256;
-        gen_try_test_circuit!({ COUNT as usize });
-        try_test_circuit((0..COUNT).collect());
+        const CNT: u64 = 256;
+        const NUM: usize = 16; // sqrt of 256
+        gen_try_test_circuit!({ CNT as usize }, NUM, NUM);
+        try_test_circuit((0..CNT).collect(), false);
+    }
+
+    #[test]
+    fn unusable_selector() {
+        const CNT: u64 = 100;
+        const NUM: usize = 16; // sqrt of 256
+        gen_try_test_circuit!({ CNT as usize }, NUM, NUM);
+        try_test_circuit((0..CNT).collect(), false);
+        try_test_circuit((0..(NUM * NUM) as u64).collect(), true);
     }
 }
