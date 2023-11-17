e0111f104584b567c0f864f728006185b8298c43
geruiwang <wang.gerui@foxmail.com>
2023-11-17 19:11:56 +0800

    remove exceedlength

--- zkevm-circuits/src/execution/returndatacopy.rs
+++ zkevm-circuits/src/execution/returndatacopy.rs
@@ -101,8 +101,6 @@ impl<F: Field, const NUM_STATE_HI_COL: usize, const NUM_STATE_LO_COL: usize>
         let is_zero_len =
             SimpleIsZero::new(&stack_pop_values[2], &len_lo_inv, String::from("lengthlo"));
 
-        let is_copydata_exceed_len = meta.query_advice(config.vers[25], Rotation::prev());
-
         constraints.append(&mut config.get_copy_contraints(
             copy::Type::Returndata,
             call_id.clone(),
@@ -123,10 +121,6 @@ impl<F: Field, const NUM_STATE_HI_COL: usize, const NUM_STATE_LO_COL: usize>
                 opcode - (OpcodeId::RETURNDATACOPY).expr(),
             ),
             ("next pc ".into(), pc_next - pc_cur - PC_DELTA.expr()),
-            (
-                "copydata not exceed returndate length".into(),
-                is_copydata_exceed_len.expr(),
-            ),
         ]);
         // TODO: add return data length > copy size
 
