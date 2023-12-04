eb183acbc9ddb1e0f7fb40848261cb88ec9e9a7f
geruiwang <wang.gerui@foxmail.com>
2023-12-04 15:32:53 +0800

    prepare integration tests for each opcodes

---
+++ zkevm-circuits/tests/opcode_tests/add.rs
@@ -0,0 +1,23 @@
+use crate::test_super_circuit_short_bytecode;
+use eth_types::{bytecode, U256};
+
+#[test]
+fn test_from_test_data() {
+    let a = U256::from_str_radix(
+        "0xff03210321032103210303210321032103210321032103210321032103210321",
+        16,
+    )
+    .unwrap();
+    let b = U256::from_str_radix(
+        "0xff10321032103210321032103210321032103210321032103210321032103210",
+        16,
+    )
+    .unwrap();
+    let bytecode = bytecode! {
+        PUSH32(a)
+        PUSH32(b)
+        ADD
+        STOP
+    };
+    test_super_circuit_short_bytecode!(bytecode);
+}
