d6b5966388f5c9c432021aabe531e22013bf53ad
gby2023 <gby1007@outlook.com>
2023-12-15 18:24:27 +0800

    refine comparsion tests

--- zkevm-circuits/tests/opcode_tests/gt.rs
+++ zkevm-circuits/tests/opcode_tests/gt.rs
@@ -2,7 +2,7 @@ use crate::test_super_circuit_short_bytecode;
 use eth_types::{bytecode, U256};
 
 #[test]
-fn gt1_bytecode() {
+fn gt0_bytecode() {
     let a = U256::from_str_radix("10", 10).unwrap();
     let b = U256::from_str_radix("9", 10).unwrap();
     let bytecode = bytecode! {
@@ -15,7 +15,20 @@ fn gt1_bytecode() {
 } // a>b
 
 #[test]
-fn gt0_bytecode() {
+fn gt1_bytecode() {
+    let a = U256::from_str_radix("9", 10).unwrap();
+    let b = U256::from_str_radix("10", 10).unwrap();
+    let bytecode = bytecode! {
+        PUSH1(b)
+        PUSH1(a)
+        GT // a>b : 1 if a is bigger, 0 otherwise
+        STOP
+    };
+    test_super_circuit_short_bytecode!(bytecode);
+} // a<b
+
+#[test]
+fn gt2_bytecode() {
     let a = U256::from_str_radix("10", 10).unwrap();
     let b = U256::from_str_radix("10", 10).unwrap();
     let bytecode = bytecode! {
@@ -25,4 +38,4 @@ fn gt0_bytecode() {
         STOP
     };
     test_super_circuit_short_bytecode!(bytecode);
-} // a<=b
+} // a=b
