77c7011fa36d40a69839f45df5763f8c7e1e3d92
gby2023 <gby1007@outlook.com>
2023-12-15 18:34:36 +0800

    refine comparsion tests

--- zkevm-circuits/tests/opcode_tests/sgt.rs
+++ zkevm-circuits/tests/opcode_tests/sgt.rs
@@ -15,7 +15,20 @@ fn sgt0_bytecode() {
 } // a>b
 
 #[test]
-fn sgt0_bytecode() {
+fn sgt1_bytecode() {
+    let a = U256::from_str_radix("10", 10).unwrap();
+    let b = U256::max_value();
+    let bytecode = bytecode! {
+        PUSH1(b)
+        PUSH1(a)
+        SGT // a>b : 1 if a is bigger, 0 otherwise
+        STOP
+    };
+    test_super_circuit_short_bytecode!(bytecode);
+} // a<b
+
+#[test]
+fn sgt2_bytecode() {
     let a = U256::from_str_radix("10", 10).unwrap();
     let b = U256::from_str_radix("10", 10).unwrap();
     let bytecode = bytecode! {
