8b0cb9cafec3decb53c07ac7b33b711b0da034c9
gby2023 <gby1007@outlook.com>
2023-12-22 11:47:45 +0800

    pass calldata to marco test_super_circuit_short_bytecode!()

--- zkevm-circuits/tests/opcode_tests/calldatacopy.rs
+++ zkevm-circuits/tests/opcode_tests/calldatacopy.rs
@@ -15,5 +15,16 @@ fn calldatacopy_bytecode() {
     let (witness, ..) = test_super_circuit_short_bytecode!(bytecode, calldata);
     let mut buf = std::io::BufWriter::new(std::fs::File::create("demo.html").unwrap());
     witness.write_html(&mut buf);
-    // test_super_circuit_short_bytecode!(bytecode);
+}
+
+#[test]
+fn calldatacopy_without_calldata_bytecode() {
+    let bytecode = bytecode! {
+        PUSH1(1)
+        PUSH1(2)
+        PUSH1(0)
+        CALLDATACOPY
+        STOP
+    };
+    test_super_circuit_short_bytecode!(bytecode);
 }
--- zkevm-circuits/tests/opcode_tests/calldataload.rs
+++ zkevm-circuits/tests/opcode_tests/calldataload.rs
@@ -12,5 +12,14 @@ fn calldataload_bytecode() {
     let (witness, ..) = test_super_circuit_short_bytecode!(bytecode, calldata);
     let mut buf = std::io::BufWriter::new(std::fs::File::create("demo.html").unwrap());
     witness.write_html(&mut buf);
-    // test_super_circuit_short_bytecode!(bytecode);
+}
+
+#[test]
+fn calldataload_without_calldata_bytecode() {
+    let bytecode = bytecode! {
+        PUSH1(31)
+        CALLDATALOAD
+        STOP
+    };
+    test_super_circuit_short_bytecode!(bytecode);
 }
