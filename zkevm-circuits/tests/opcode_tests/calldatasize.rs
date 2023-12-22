bd81ede80948d542126ba8122cd4261473892381
gby2023 <gby1007@outlook.com>
2023-12-22 11:47:45 +0800

    pass calldata to marco test_super_circuit_short_bytecode!()

--- zkevm-circuits/tests/opcode_tests/calldatasize.rs
+++ zkevm-circuits/tests/opcode_tests/calldatasize.rs
@@ -11,5 +11,13 @@ fn calldatasize_bytecode() {
     let (witness, ..) = test_super_circuit_short_bytecode!(bytecode, calldata);
     let mut buf = std::io::BufWriter::new(std::fs::File::create("demo.html").unwrap());
     witness.write_html(&mut buf);
-    // test_super_circuit_short_bytecode!(bytecode);
+}
+
+#[test]
+fn calldatasize_without_calldata_bytecode() {
+    let bytecode = bytecode! {
+        CALLDATASIZE
+        STOP
+    };
+    test_super_circuit_short_bytecode!(bytecode);
 }
