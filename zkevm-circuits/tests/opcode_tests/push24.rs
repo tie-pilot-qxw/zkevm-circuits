b892d486a731181c2751fdb670cd7ca1ae690e3d
gby2023 <gby1007@outlook.com>
2023-12-26 12:54:17 +0800

    remove unpassed tests

--- zkevm-circuits/tests/opcode_tests/push24.rs
+++ zkevm-circuits/tests/opcode_tests/push24.rs
@@ -1,5 +1,5 @@
+use crate::gen_random_hex_str;
 use crate::get_func_name;
-use crate::test;
 use crate::test_super_circuit_short_bytecode;
 use eth_types::{bytecode, U256};
 
@@ -8,8 +8,8 @@ fn push24_bytecode() {
     let func_name = get_func_name!().to_string();
     let my_vec: Vec<char> = func_name.chars().collect();
     let num_byte = usize::try_from(my_vec[4].to_digit(10).unwrap()).unwrap();
-    let str1 = test::gen_random_hex_str(num_byte * 2);
-    let str2 = test::gen_random_hex_str(num_byte * 2);
+    let str1 = gen_random_hex_str(num_byte * 2);
+    let str2 = gen_random_hex_str(num_byte * 2);
 
     let first = U256::from_str_radix(&str1, 16).unwrap();
     let second = U256::from_str_radix(&str2, 16).unwrap();
