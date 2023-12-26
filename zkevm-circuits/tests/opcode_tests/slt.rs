cccd135129d9d20d1598ad2eae10af2127603329
gby2023 <gby1007@outlook.com>
2023-12-26 16:53:03 +0800

    add tests

--- zkevm-circuits/tests/opcode_tests/slt.rs
+++ zkevm-circuits/tests/opcode_tests/slt.rs
@@ -12,10 +12,23 @@ fn slt0_bytecode() {
         STOP
     };
     test_super_circuit_short_bytecode!(bytecode);
-}// a<b
+} // a<b
 
 #[test]
-fn slt0_bytecode() {
+fn slt1_bytecode() {
+    let a = U256::from_str_radix("10", 10).unwrap();
+    let b = U256::max_value();
+    let bytecode = bytecode! {
+        PUSH32(b)
+        PUSH1(a)
+        SLT // a>b : 1 if a is smaller, 0 otherwise
+        STOP
+    };
+    test_super_circuit_short_bytecode!(bytecode);
+} // a>b
+
+#[test]
+fn slt2_bytecode() {
     let a = U256::from_str_radix("10", 10).unwrap();
     let b = U256::from_str_radix("10", 10).unwrap();
     let bytecode = bytecode! {
@@ -25,4 +38,4 @@ fn slt0_bytecode() {
         STOP
     };
     test_super_circuit_short_bytecode!(bytecode);
-} // a>=b
+} // a=b
