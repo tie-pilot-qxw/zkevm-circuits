4945ff0cee61e7a399c082c62f35dac71cbf1239
geruiwang <wang.gerui@foxmail.com>
2024-01-15 16:31:36 +0800

    feat: add index impl for core vers --story=0

--- zkevm-circuits/src/witness/core.rs
+++ zkevm-circuits/src/witness/core.rs
@@ -3,6 +3,7 @@ use eth_types::evm_types::OpcodeId;
 use eth_types::U256;
 use serde::Serialize;
 use std::collections::HashMap;
+use std::ops::{Index, IndexMut};
 
 /// core row has a few single-purpose columns (tx_idx, call_id, code_addr, pc, opcode, cnt)
 /// and 32 versatile columns
@@ -59,4 +60,84 @@ pub struct Row {
     /// comments to show in html table that explain the purpose of each cell
     #[serde(skip_serializing)]
     pub comments: HashMap<String, String>,
+}
+
+impl Index<usize> for Row {
+    type Output = Option<U256>;
+    fn index<'a>(&'a self, i: usize) -> &'a Option<U256> {
+        match i {
+            0 => &self.vers_0,
+            1 => &self.vers_1,
+            2 => &self.vers_2,
+            3 => &self.vers_3,
+            4 => &self.vers_4,
+            5 => &self.vers_5,
+            6 => &self.vers_6,
+            7 => &self.vers_7,
+            8 => &self.vers_8,
+            9 => &self.vers_9,
+            10 => &self.vers_10,
+            11 => &self.vers_11,
+            12 => &self.vers_12,
+            13 => &self.vers_13,
+            14 => &self.vers_14,
+            15 => &self.vers_15,
+            16 => &self.vers_16,
+            17 => &self.vers_17,
+            18 => &self.vers_18,
+            19 => &self.vers_19,
+            20 => &self.vers_20,
+            21 => &self.vers_21,
+            22 => &self.vers_22,
+            23 => &self.vers_23,
+            24 => &self.vers_24,
+            25 => &self.vers_25,
+            26 => &self.vers_26,
+            27 => &self.vers_27,
+            28 => &self.vers_28,
+            29 => &self.vers_29,
+            30 => &self.vers_30,
+            31 => &self.vers_31,
+            _ => panic!("core.vers index out of bound"),
+        }
+    }
+}
+impl IndexMut<usize> for Row {
+    fn index_mut<'a>(&'a mut self, i: usize) -> &'a mut Option<U256> {
+        match i {
+            0 => &mut self.vers_0,
+            1 => &mut self.vers_1,
+            2 => &mut self.vers_2,
+            3 => &mut self.vers_3,
+            4 => &mut self.vers_4,
+            5 => &mut self.vers_5,
+            6 => &mut self.vers_6,
+            7 => &mut self.vers_7,
+            8 => &mut self.vers_8,
+            9 => &mut self.vers_9,
+            10 => &mut self.vers_10,
+            11 => &mut self.vers_11,
+            12 => &mut self.vers_12,
+            13 => &mut self.vers_13,
+            14 => &mut self.vers_14,
+            15 => &mut self.vers_15,
+            16 => &mut self.vers_16,
+            17 => &mut self.vers_17,
+            18 => &mut self.vers_18,
+            19 => &mut self.vers_19,
+            20 => &mut self.vers_20,
+            21 => &mut self.vers_21,
+            22 => &mut self.vers_22,
+            23 => &mut self.vers_23,
+            24 => &mut self.vers_24,
+            25 => &mut self.vers_25,
+            26 => &mut self.vers_26,
+            27 => &mut self.vers_27,
+            28 => &mut self.vers_28,
+            29 => &mut self.vers_29,
+            30 => &mut self.vers_30,
+            31 => &mut self.vers_31,
+            _ => panic!("core.vers index out of bound"),
+        }
+    }
 }
