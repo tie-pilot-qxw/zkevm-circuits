3a906b84c43dfb8a1dd7379b66fa2a59b6aa107d
韩志超 <han_zhichao@sina.cn>
2024-01-16 16:11:41 +0800

    fix: fix mulmod AuxiliaryOutcome and tests/opcode_tests/main.rs

--- zkevm-circuits/tests/opcode_tests/main.rs
+++ zkevm-circuits/tests/opcode_tests/main.rs
@@ -127,9 +127,13 @@ macro_rules! test_super_circuit_short_bytecode {
     ($bytecode:expr) => {{
         use halo2_proofs::dev::MockProver;
         use halo2_proofs::halo2curves::bn256::Fr;
+        use zkevm_circuits::constant::{
+            MAX_CODESIZE, MAX_NUM_ROW, NUM_STATE_HI_COL, NUM_STATE_LO_COL,
+        };
         use zkevm_circuits::super_circuit::SuperCircuit;
         use zkevm_circuits::util::{geth_data_test, log2_ceil, SubCircuit};
         use zkevm_circuits::witness::Witness;
+
         let machine_code = $bytecode.to_vec();
         let trace = trace_parser::trace_program(&machine_code, &[]);
         let witness = Witness::new(&geth_data_test(
@@ -141,7 +145,14 @@ macro_rules! test_super_circuit_short_bytecode {
         ));
 
         let k = 9;
-        let circuit = SuperCircuit::<Fr, 490, 480, 10, 10>::new_from_witness(&witness);
+        // let circuit = SuperCircuit::<Fr, 490, 480, 10, 10>::new_from_witness(&witness);
+        let circuit: SuperCircuit<
+            Fr,
+            MAX_NUM_ROW,
+            MAX_CODESIZE,
+            NUM_STATE_HI_COL,
+            NUM_STATE_LO_COL,
+        > = SuperCircuit::new_from_witness(&witness);
         let instance = circuit.instance();
         let prover = MockProver::<Fr>::run(k, &circuit, instance).unwrap();
         let file_name = std::path::Path::new(file!()).file_stem().unwrap();
@@ -159,9 +170,13 @@ macro_rules! test_super_circuit_short_bytecode {
         let calldata = hex::decode($calldata).expect("calldata should be hex string");
         use halo2_proofs::dev::MockProver;
         use halo2_proofs::halo2curves::bn256::Fr;
+        use zkevm_circuits::constant::{
+            MAX_CODESIZE, MAX_NUM_ROW, NUM_STATE_HI_COL, NUM_STATE_LO_COL,
+        };
         use zkevm_circuits::super_circuit::SuperCircuit;
         use zkevm_circuits::util::{geth_data_test, log2_ceil, SubCircuit};
         use zkevm_circuits::witness::Witness;
+
         let machine_code = $bytecode.to_vec();
         let trace = trace_parser::trace_program(&machine_code, &calldata);
         let witness = Witness::new(&geth_data_test(
@@ -173,7 +188,14 @@ macro_rules! test_super_circuit_short_bytecode {
         ));
 
         let k = log2_ceil(501);
-        let circuit = SuperCircuit::<Fr, 490, 480, 10, 10>::new_from_witness(&witness);
+        // let circuit = SuperCircuit::<Fr, 490, 480, 10, 10>::new_from_witness(&witness);
+        let circuit: SuperCircuit<
+            Fr,
+            MAX_NUM_ROW,
+            MAX_CODESIZE,
+            NUM_STATE_HI_COL,
+            NUM_STATE_LO_COL,
+        > = SuperCircuit::new_from_witness(&witness);
         let instance = circuit.instance();
         let prover = MockProver::<Fr>::run(k, &circuit, instance).unwrap();
         if prover.verify_par().is_err() {
