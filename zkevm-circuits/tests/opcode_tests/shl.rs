ce8824ade49b5097b13ebff0f38484d08912e4cf
geruiwang <wang.gerui@foxmail.com>
2024-01-19 14:48:02 +0800

    chore: make test faster --story=1016276

--- zkevm-circuits/tests/opcode_tests/shl.rs
+++ zkevm-circuits/tests/opcode_tests/shl.rs
@@ -4,7 +4,7 @@ use eth_types::{bytecode, U256};
 #[test]
 fn shl_bytecode() {
     let value = U256::from_str_radix("0x553e92e8bc0ae9a795ed1f57f3632d4d", 16).unwrap();
-    for shift in (0..256).step_by(16) {
+    for shift in [0, 1, 31, 32, 255, 256] {
         let bytecode = bytecode! {
             PUSH32(value)
             PUSH32(shift)
