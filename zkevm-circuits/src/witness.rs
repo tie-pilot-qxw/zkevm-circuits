44d42285bc37388a1cdb672139f0b26785c12fb9
xiaoranlu <xiaoranlu@tencent.com>
2024-05-23 11:56:13 +0800

    feat: implement most significant byte length --story=1017675

--- zkevm-circuits/src/witness.rs
+++ zkevm-circuits/src/witness.rs
@@ -16,9 +16,10 @@ use crate::constant::{
     ARITHMETIC_COLUMN_WIDTH, ARITHMETIC_TINY_COLUMN_WIDTH, ARITHMETIC_TINY_START_IDX,
     BITWISE_COLUMN_START_IDX, BITWISE_COLUMN_WIDTH, BIT_SHIFT_MAX_IDX, BYTECODE_COLUMN_START_IDX,
     COPY_LOOKUP_COLUMN_CNT, DESCRIPTION_AUXILIARY, EXP_COLUMN_START_IDX,
-    LOG_SELECTOR_COLUMN_START_IDX, MAX_CODESIZE, MAX_NUM_ROW, NUM_STATE_HI_COL, NUM_STATE_LO_COL,
-    NUM_VERS, PUBLIC_COLUMN_START_IDX, PUBLIC_COLUMN_WIDTH, PUBLIC_NUM_VALUES,
-    STAMP_CNT_COLUMN_START_IDX, STATE_COLUMN_WIDTH, STORAGE_COLUMN_WIDTH,
+    LOG_SELECTOR_COLUMN_START_IDX, MAX_CODESIZE, MAX_NUM_ROW,
+    MOST_SIGNIFICANT_BYTE_LEN_COLUMN_WIDTH, NUM_STATE_HI_COL, NUM_STATE_LO_COL, NUM_VERS,
+    PUBLIC_COLUMN_START_IDX, PUBLIC_COLUMN_WIDTH, PUBLIC_NUM_VALUES, STAMP_CNT_COLUMN_START_IDX,
+    STATE_COLUMN_WIDTH, STORAGE_COLUMN_WIDTH,
 };
 use crate::copy_circuit::CopyCircuit;
 use crate::core_circuit::CoreCircuit;
@@ -2329,6 +2330,37 @@ impl core::Row {
             (format!("vers_{}", index * 5 + 2), "acc_1".into()),
             (format!("vers_{}", index * 5 + 3), "acc_2".into()),
             (format!("vers_{}", index * 5 + 4), "sum_2".into()),
+        ]);
+    }
+    pub fn insert_most_significant_byte_len_lookups(
+        &mut self,
+        index: usize,
+        bitwise_row: &bitwise::Row,
+    ) {
+        assert!(index <= 3);
+        assert_eq!(self.cnt, 2.into());
+        let column_values = [bitwise_row.acc_2, bitwise_row.index];
+        for i in 0..MOST_SIGNIFICANT_BYTE_LEN_COLUMN_WIDTH {
+            assign_or_panic!(
+                self[BITWISE_COLUMN_START_IDX + MOST_SIGNIFICANT_BYTE_LEN_COLUMN_WIDTH * index + i],
+                column_values[i]
+            );
+        }
+        self.comments.extend([
+            (
+                format!(
+                    "vers_{}",
+                    BITWISE_COLUMN_START_IDX + MOST_SIGNIFICANT_BYTE_LEN_COLUMN_WIDTH * index
+                ),
+                "acc_2".into(),
+            ),
+            (
+                format!(
+                    "vers_{}",
+                    BITWISE_COLUMN_START_IDX + MOST_SIGNIFICANT_BYTE_LEN_COLUMN_WIDTH * index + 1
+                ),
+                "index".into(),
+            ),
         ]);
     }
     pub fn insert_state_lookups<const NUM_LOOKUP: usize>(
